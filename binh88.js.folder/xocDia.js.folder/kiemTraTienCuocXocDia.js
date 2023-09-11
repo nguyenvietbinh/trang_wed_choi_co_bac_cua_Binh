43b44d569346da3f38d7d0792eb420e4ccf8fb59
nguyenvietbinh <126882221+nguyenvietbinh@users.noreply.github.com>
2023-09-11 08:56:37 +0000

    kiem tra tien cuoc day 2

--- binh88.js.folder/xocDia.js.folder/kiemTraTienCuocXocDia.js
+++ binh88.js.folder/xocDia.js.folder/kiemTraTienCuocXocDia.js
@@ -1,7 +1,36 @@
 
 let run = false
-kiemTraTienCuocXocDia = function(listOfInput) {
-    for (let i in listOfInput) {
 
+
+for (let i = 0; i <= 5; i++) {
+    xocDiaInputElements[i].addEventListener('blur', function() {
+        console.log('a')
+    })
+}
+kiemTraTienCuocXocDia = function(listOfInput) {
+
+}
+daDatCuocChua = function (listToCheck) {
+    for (let i in listToCheck) {
+        if (listToCheck[i] !== '') {
+            return true
+        }
+    }
+    return false
+}
+kiemTraKieuDuLieu = function (variableToCheck, limmit) {
+    if (!isNaN(variableToCheck)) {
+        if (parseInt(variableToCheck) === 0) {
+            alert('so tien khong hop le!')
+            return false
+        } else if (parseInt(variableToCheck) > limmit){
+            alert('tien cuoc qua lon!')
+            return false
+        } else {
+            return true
+        }
+    } else {
+        alert('so tien khong hop le!')
+        return false
     }
 }
